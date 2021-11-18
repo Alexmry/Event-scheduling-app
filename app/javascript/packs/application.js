bfe75140592fe8485ebc4bf11679bbff7c764a23
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-18 16:23:27 +0000

    view set up

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,10 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 import "stylesheets/application"
+
+
+//= require rails-ujs
+//= require activestorage
+//= require trix
+//= require turbolinks
+//= require_tree .
